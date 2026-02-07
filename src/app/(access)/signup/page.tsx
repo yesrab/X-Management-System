4e70f257d67ad1a085b411bbd246a4c62a14ed49
yesrab <yesrab.ali111@gmail.com>
2026-02-07 22:52:57 +0530

    chore: added basic non functional login and signup page

--- src/app/(access)/signup/page.tsx
+++ src/app/(access)/signup/page.tsx
@@ -1,5 +1,11 @@
-const SignupPage = () => {
-  return <div>page</div>;
-};
+import { SignupForm } from "@/components/access/signup-form";
 
-export default SignupPage;
+export default function SignupPage() {
+  return (
+    <div className='bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10'>
+      <div className='w-full max-w-sm md:max-w-4xl'>
+        <SignupForm />
+      </div>
+    </div>
+  );
+}
