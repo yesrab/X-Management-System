88937020004021d3064a6a3cedb67d5f44f9fb01
yesrab <yesrab.ali111@gmail.com>
2026-02-15 21:48:47 +0530

    updated paths

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,9 +4,9 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "./src/prisma/schema.prisma",
+  schema: "@/prisma/schema.prisma",
   migrations: {
-    path: "./src/prisma/migrations",
+    path: "@/prisma/migrations",
   },
   datasource: {
     url: process.env["DATABASE_URL"],
