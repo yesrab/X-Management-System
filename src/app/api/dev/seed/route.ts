01677de6e7b250b13eea217997ae272a92f878c5
yesrab <yesrab.ali111@gmail.com>
2026-03-07 21:20:59 +0530

    chore: updated seeder for prod

--- src/app/api/dev/seed/route.ts
+++ src/app/api/dev/seed/route.ts
@@ -1,15 +1,36 @@
-import { NextResponse } from "next/server";
-import { main } from "@/prisma/seed";
+import { NextResponse, NextRequest } from 'next/server';
+import { main } from '@/prisma/seed';
+import logger from '@/lib/logger';
 
-export async function POST() {
-  if (process.env.NODE_ENV === "production") {
-    return NextResponse.json({ error: "Not allowed in production" }, { status: 403 });
+const isProduction = process.env.NODE_ENV === 'production';
+const sessionSecret = process.env.SESSION_SECRET;
+const canUsedSecret = sessionSecret && sessionSecret !== 'danger';
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const { secret } = body;
+
+  if (isProduction) {
+    if (canUsedSecret || secret === sessionSecret) {
+      logger.info('Authorized seeding request received in production');
+    } else {
+      return NextResponse.json(
+        { error: 'Not allowed in production' },
+        { status: 403 },
+      );
+    }
   }
   try {
     await main();
-    return NextResponse.json({ success: true, message: "Database reset and seeded successfully" });
+    return NextResponse.json({
+      success: true,
+      message: 'Database reset and seeded successfully',
+    });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: "Seeding failed" }, { status: 500 });
+    return NextResponse.json(
+      { success: false, message: 'Seeding failed' },
+      { status: 500 },
+    );
   }
 }
